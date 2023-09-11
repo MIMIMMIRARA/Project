ee6b473c75ddda1cd525894d43232c3b26936c0f
len1211 <len1211@naver.com>
2023-09-11 21:13:20 +0900

    login

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,15 +22,15 @@ public class SecurityConfig  {
 
     @Bean
     public SecurityFilterChain filterChanin(HttpSecurity http) throws Exception {
-//        http.formLogin()
-//                .loginPage("members/login")
-//                .defaultSuccessUrl("/")
-//                .usernameParameter("email");
-//                .failureUrl("members/login/error")
-//                .and()
-//                .logout()
-//                .logoutRequestMatcher(new AntPathRequestMatcher("members/logout"))
-//                .logoutSuccessUrl("/");
+        http.formLogin()
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/");
 
         http.authorizeRequests()
                 .mvcMatchers("/css/**", "/js/**", "/img/**").permitAll()
@@ -37,8 +38,9 @@ public class SecurityConfig  {
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated();
 
-//        http.exceptionHandling()
-//                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+        http.exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+
 
         http.csrf().disable();
 
--- src/main/java/com/shop/constant/Role.java
+++ src/main/java/com/shop/constant/Role.java
@@ -1,5 +1,5 @@
 package com.shop.constant;
 
 public enum Role {
-    USER, ADMINM
+    USER, ADMIN
 }
