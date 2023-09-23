8d314578c5c4b1285f315973a6c5aac84d86d0e3
len1211 <len1211@naver.com>
2023-09-23 10:03:13 +0900

    login

--- src/main/java/com/shop/entity/Cart.java
+++ src/main/java/com/shop/entity/Cart.java
@@ -10,14 +10,20 @@ import javax.persistence.*;
 @Table(name = "cart")
 @Getter @Setter
 @ToString
-public class Cart {
+public class Cart extends BaseEntity {
 
     @Id
     @Column(name = "cart_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    public static Cart createCart(Member member) {
+        Cart cart = new Cart();
+        cart.setMember(member);
+        return cart;
+    }
 }
